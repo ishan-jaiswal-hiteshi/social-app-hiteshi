89604b45f294945d1091e9222481ef8000241ef1
ishan jaiswal <ishan.jaiswal@hiteshi.com>
2024-12-25 10:53:01 +0530

    profile and auth update

--- src/components/dashboard/layout/sidebar.tsx
+++ src/components/dashboard/layout/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
@@ -57,7 +59,11 @@ const SidebarLayout = () => {
 
         <div>
           <Link
-            href="/dashboard/profile"
+            href="/"
+            onClick={() => {
+              // Clear the accessToken from localStorage
+              localStorage.removeItem("accessToken");
+            }}
             className="hover:text-gray-300 cursor-pointer flex gap-2 items-center"
           >
             <svg
@@ -65,11 +71,11 @@ const SidebarLayout = () => {
               height="24px"
               viewBox="0 -960 960 960"
               width="24px"
-              fill="#fff"
+              fill="red"
             >
               <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
             </svg>
-            <p>Log out</p>
+            <p className="text-red-500">Log out</p>
           </Link>
         </div>
       </div>
